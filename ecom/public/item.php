c1d10cdd77120d2ca40300573efb6d3e4c596d90
mv98 <49610618+mv98@users.noreply.github.com>
2019-04-14 14:53:20 +0200

    Add files via upload

---
+++ ecom/public/item.php
@@ -0,0 +1,109 @@
+<?php require_once("../resources/config.php"); ?>
+
+<?php include(TEMPLATE_FRONT . DS . "header.php"); ?>
+
+<div class="container">
+
+       <?php include(TEMPLATE_FRONT . DS . "side_nav.php"); ?>
+
+
+      <?php
+      $id_product=$_GET["id"];
+       $query=query("SELECT * FROM products WHERE product_id='$id_product'");
+        confirm($query);
+
+        while($row=fetch_array($query)):
+        
+            ?>
+
+
+<div class="col-md-9">
+
+
+<div class="row">
+
+    <div class="col-md-7">
+       <img class="img-responsive" src="<?php echo $row['product_image']; ?>" alt="">
+
+    </div>
+
+    <div class="col-md-5">
+
+        <div class="thumbnail">
+         
+
+    <div class="caption-full">
+        <h4><a href="#"><?php echo $row['product_title']; ?></a> </h4>
+        <hr>
+        <h4 class=""><?php echo  "$" . $row['product_price']; ?></h4>
+
+    <div class="ratings">
+     
+        <p>
+            <span class="glyphicon glyphicon-star"></span>
+            <span class="glyphicon glyphicon-star"></span>
+            <span class="glyphicon glyphicon-star"></span>
+            <span class="glyphicon glyphicon-star"></span>
+            <span class="glyphicon glyphicon-star-empty"></span>
+            4.0 stars
+        </p>
+    </div>
+          
+        <p><?php echo $row['short_desc']; ?></p>
+
+   
+    <form action="">
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="ADD TO CART">
+        </div>
+    </form>
+
+    </div>
+ 
+</div>
+
+</div>
+
+
+</div><!--Row For Image and Short Description-->
+
+
+        <hr>
+
+
+<!--Row for Tab Panel-->
+
+<div class="row">
+
+<div role="tabpanel">
+
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Description</a></li>
+    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Reviews</a></li>
+
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="home">
+
+<p>
+
+<?php echo $row['product_description']; ?>
+
+</p>
+    
+    </div>
+   
+
+
+</div><!-- col-md-9 ends here -->
+
+        <?php endwhile; ?>
+
+</div>
+    <!-- /.container -->
+
+    <?php include(TEMPLATE_FRONT . DS . "footer.php"); ?>
+ 
